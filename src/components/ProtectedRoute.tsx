96e475822f9c6f46fe1306163a088e2c76d2f819
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-02-14 13:14:00 +0100

    skyddade routes

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,22 @@
+import { Navigate } from "react-router-dom";
+import type { ReactNode } from "react";
+import { useAuth } from "../context/AuthContext";
+
+interface ProtectedRouteProps {
+    children: ReactNode,
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+    
+    const { user } = useAuth();
+
+    if (!user) {
+        return <Navigate to="/login" replace />
+    }
+
+    return (
+        <>{children}</>
+    )
+}
+
+export default ProtectedRoute
