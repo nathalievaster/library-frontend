a7b4997bd81400eb39b6da05472c9a6c7e1ccbdb
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-02-14 08:16:19 +0100

    en layout-fil

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,6 @@ import Header from "../components/Header"
 const HomePage = () => {
   return (
     <>
-      <Header />
       <h1>HomePage</h1>
     </>
   )
--- src/routing.tsx
+++ src/routing.tsx
@@ -3,23 +3,30 @@ import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import AdminPage from "./pages/AdminPage";
 import BookDetailsPage from "./pages/BookDetailsPage";
+import Layout from "./components/Layout";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <HomePage />
-    },
-    {
-        path: "/login",
-        element: <LoginPage />
-    },
-    {
-        path: "/admin",
-        element: <AdminPage />
-    },
-    {
-        path: "/books/:id",
-        element: <BookDetailsPage />
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <HomePage />
+            },
+            {
+                path: "/login",
+                element: <LoginPage />
+            },
+            {
+                path: "/admin",
+                element: <AdminPage />
+            },
+            {
+                path: "/books/:id",
+                element: <BookDetailsPage />
+            }
+        ]
     }
 ])
 
