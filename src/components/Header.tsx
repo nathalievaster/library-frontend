fe472e2ac6570a0f42c355a308e9b32235e3a7a6
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-02-14 12:06:38 +0100

    enkel grundläggande style på header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import { NavLink } from "react-router-dom"
+import styles from "./Header.module.css"
 
 const Header = () => {
   return (
-    <header>
+    <header className={styles.header}>
         <ul>
             <li><NavLink to="/">Startsida</NavLink></li>
             <li><NavLink to="/login">Logga in</NavLink></li>
