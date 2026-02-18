2ff5300a006272b2bc646f06cdf8f72e43a28f55
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-02-18 13:31:12 +0100

    Hämtar böcker från API och visar dem på startsidan

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,51 @@
+import { use, useEffect, useState } from "react"
+import type { Book } from "../types/book.types"
+
 const HomePage = () => {
+
+  // States för att hantera böcker, laddning och fel
+  const [books, setBooks] = useState<Book[]>([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
+
+  // useEffect för att hämta böcker när komponenten mountas
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/api/books")
+
+        if (!res.ok) {
+          throw new Error("Kunde inte hämta böcker.")
+        }
+
+        const data = await res.json()
+        setBooks(data)
+      } catch (err) {
+        setError("Något gick fel...")
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchBooks()
+    // kör bara en gång när komponenten mountas, därför tom array som dependency
+  }, [])
+
+  if (loading) return <p>Laddar...</p>
+  if (error) return <p>{error}</p>
+
   return (
     <>
-      <h1>HomePage</h1>
+      <h1>Homepage</h1>
+
+      {books.map(book => (
+        <article key={book._id}>
+          <h2>{book.title}</h2>
+          <p><strong>Författare:</strong> {book.author}</p>
+          {book.description && <p><strong>Beskrivning:</strong> {book.description}</p>}
+          {book.publishedYear && <p><strong>Utgivningsår:</strong> {book.publishedYear}</p>}
+        </article>
+      ))}
     </>
   )
 }
